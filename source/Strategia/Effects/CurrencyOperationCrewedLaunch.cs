b91d8fc2cab683fc3aaa848e10b30e5d1ad5c74c
Jeremie Rossignol <jrossignol@gmail.com>
2016-01-04 21:30:57 -0700

    Add downside to Probe Frenzy.

---
+++ source/Strategia/Effects/CurrencyOperationCrewedLaunch.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Strategies;
+using Strategies.Effects;
+
+namespace Strategia
+{
+    /// <summary>
+    /// Special CurrencyOperation that gives a modifier for crewed launches.
+    /// </summary>
+    public class CurrencyOperationCrewedLaunch : StrategyEffect
+    {
+        List<Currency> currencies;
+        string effectDescription;
+        List<TransactionReasons> affectReasons;
+        List<float> multipliers;
+        bool shipManned = false;
+
+        public CurrencyOperationCrewedLaunch(Strategy parent)
+            : base(parent)
+        {
+            Debug.Log("yyy CurrencyOperationCrewedLaunch");
+        }
+
+        protected override string GetDescription()
+        {
+            float multiplier = Parent.GetLeveledListItem(multipliers);
+            string multiplierStr = ToPercentage(multiplier);
+
+            string currencyStr = currencies.Count() > 1 ? "" : (currencies.First() + " ");
+
+            return "Cost to launch crewed vessels increased by " + multiplierStr;
+        }
+
+        protected override void OnLoadFromConfig(ConfigNode node)
+        {
+            base.OnLoadFromConfig(node);
+
+            currencies = ConfigNodeUtil.ParseValue<List<Currency>>(node, "currency");
+            effectDescription = ConfigNodeUtil.ParseValue<string>(node, "effectDescription");
+            affectReasons = ConfigNodeUtil.ParseValue<List<TransactionReasons>>(node, "AffectReason");
+            multipliers = ConfigNodeUtil.ParseValue<List<float>>(node, "multiplier");
+        }
+
+        protected override void OnSave(ConfigNode node)
+        {
+            base.OnSave(node);
+        }
+
+        protected override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+        }
+
+        protected override void OnRegister()
+        {
+            GameEvents.Modifiers.OnCurrencyModifierQuery.Add(new EventData<CurrencyModifierQuery>.OnEvent(OnEffectQuery));
+            GameEvents.OnVesselRollout.Add(new EventData<ShipConstruct>.OnEvent(OnVesselRollout));
+        }
+
+        protected override void OnUnregister()
+        {
+            GameEvents.Modifiers.OnCurrencyModifierQuery.Remove(new EventData<CurrencyModifierQuery>.OnEvent(OnEffectQuery));
+            GameEvents.OnVesselRollout.Add(new EventData<ShipConstruct>.OnEvent(OnVesselRollout));
+        }
+
+        private void OnVesselRollout(ShipConstruct ship)
+        {
+            shipManned = ship.Parts.Any(p => p.CrewCapacity > 0);
+        }
+
+        private void OnEffectQuery(CurrencyModifierQuery qry)
+        {
+            Debug.Log("yyy OnEffectQuery");
+            // Check the reason is a match
+            if (!affectReasons.Contains(qry.reason) || !shipManned)
+            {
+                return;
+            }
+
+            // Check if it's non-zero
+            float total = 0.0f;
+            foreach (Currency currency in currencies)
+            {
+                total += Math.Abs(qry.GetInput(currency));
+            }
+            if (total < 0.01f)
+            {
+                return;
+            }
+
+            float multiplier = Parent.GetLeveledListItem(multipliers);
+            foreach (Currency currency in currencies)
+            {
+                qry.AddDelta(currency, multiplier * qry.GetInput(currency) - qry.GetInput(currency));
+            }
+        }
+    }
+}
