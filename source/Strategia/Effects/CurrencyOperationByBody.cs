7a276106fe628fee38f1fa20c1160841fa06fa9c
Jeremie Rossignol <jrossignol@gmail.com>
2016-01-11 13:36:10 -0700

    Fixed detection of body for rewards.

--- source/Strategia/Effects/CurrencyOperationByBody.cs
+++ source/Strategia/Effects/CurrencyOperationByBody.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Text;
 using UnityEngine;
 using KSP;
+using KSPAchievements;
 using Strategies;
 using Strategies.Effects;
 
@@ -13,6 +14,8 @@ namespace Strategia
     public class CurrencyOperationByBody : CurrencyOperation
     {
         private List<CelestialBody> bodies;
+
+        private CelestialBody lastBody;
 
         public CurrencyOperationByBody(Strategy parent)
             : base(parent)
@@ -51,17 +54,41 @@ namespace Strategia
         protected override void OnRegister()
         {
             GameEvents.Modifiers.OnCurrencyModifierQuery.Add(new EventData<CurrencyModifierQuery>.OnEvent(OnEffectQuery));
+            GameEvents.OnProgressComplete.Add(new EventData<ProgressNode>.OnEvent(OnProgressComplete));
         }
 
         protected override void OnUnregister()
         {
             GameEvents.Modifiers.OnCurrencyModifierQuery.Remove(new EventData<CurrencyModifierQuery>.OnEvent(OnEffectQuery));
+            GameEvents.OnProgressComplete.Remove(new EventData<ProgressNode>.OnEvent(OnProgressComplete));
+        }
+
+        private void OnProgressComplete(ProgressNode node)
+        {
+            CelestialBodySubtree cbs = node as CelestialBodySubtree;
+            if (cbs != null)
+            {
+                lastBody = cbs.Body;
+            }
+            else
+            {
+                // Reflection hack time.  There is a member that is private that stores the celestial body
+                FieldInfo cbField = node.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(fi => fi.FieldType == typeof(CelestialBody)).FirstOrDefault();
+                if (cbField != null)
+                {
+                    lastBody = (CelestialBody)cbField.GetValue(node);
+                }
+                else
+                {
+                    lastBody = null;
+                }
+            }
         }
 
         private void OnEffectQuery(CurrencyModifierQuery qry)
         {
             // Check if it's for our body 
-            if (FlightGlobals.currentMainBody == null || !bodies.Contains(FlightGlobals.currentMainBody))
+            if (lastBody == null || !bodies.Contains(lastBody))
             {
                 return;
             }
