9de205f5c000c002061e985bc2d23188feb3df46
Jeremie Rossignol <jrossignol@gmail.com>
2016-02-03 13:48:30 -0700

    General improvements to misssions.

---
+++ source/Strategia/Requirements/IncompatibleGroupRequirement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using KSPAchievements;
+using Strategies;
+using Strategies.Effects;
+
+namespace Strategia
+{
+    public class IncompatibleGroupRequirement : StrategyEffect, IRequirementEffect
+    {
+        string group;
+        string text;
+        public IncompatibleGroupRequirement(Strategy parent)
+            : base(parent)
+        {
+        }
+
+        protected override void OnLoadFromConfig(ConfigNode node)
+        {
+            group = ConfigNodeUtil.ParseValue<string>(node, "group");
+            text = ConfigNodeUtil.ParseValue<string>(node, "text");
+        }
+
+        public string RequirementText()
+        {
+            return text;
+        }
+
+        public bool RequirementMet(out string unmetReason)
+        {
+            Strategy conflict = StrategySystem.Instance.Strategies.Where(s => s.Title != Parent.Title && s.GroupTags.First() == group && s.IsActive).FirstOrDefault();
+            unmetReason = conflict != null ? (conflict.Title + " is active") : null;
+            return conflict == null;
+        }
+    }
+}
