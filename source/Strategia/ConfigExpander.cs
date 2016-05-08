7be3db502acaf95049e81ad9a6e66904eea1e229
Jeremie Rossignol <jrossignol@gmail.com>
2016-05-08 10:43:19 -0600

    Added Custom Barn Kit check back in.

--- source/Strategia/ConfigExpander.cs
+++ source/Strategia/ConfigExpander.cs
@@ -34,13 +34,11 @@ namespace Strategia
         {
             if (ContractConfigurator.Util.Version.VerifyAssemblyVersion("CustomBarnKit", "1.0.0") == null)
             {
-                // TODO - reenable when CustomBarnKit is available
-                /*
                 var ainfoV = Attribute.GetCustomAttribute(GetType().Assembly, typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute;
                 string title = "Strategia " + ainfoV.InformationalVersion + " Message";
                 string message = "Strategia requires Custom Barn Kit to function properly.  Strategia is currently disabled, and will automatically re-enable itself when Custom Barn Kit is installed.";
                 DialogGUIButton dialogOption = new DialogGUIButton("Okay", new Callback(DoNothing), true);
-                PopupDialog.SpawnPopupDialog(new MultiOptionDialog(message, title, UISkinManager.GetSkin("default"), dialogOption), false, UISkinManager.GetSkin("default"));*/
+                PopupDialog.SpawnPopupDialog(new MultiOptionDialog(message, title, UISkinManager.GetSkin("default"), dialogOption), false, UISkinManager.GetSkin("default"));
             }
         }
 
