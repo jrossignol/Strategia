ea23e6f8903edcf33b0422068a2b64b7c9cbdb41
Jeremie Rossignol <jrossignol@gmail.com>
2017-06-11 22:12:15 -0600

    Added logic for increasing likelihood of contracts for diferent bodies.

---
+++ source/Strategia/Effects/ContractWeightBooster.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using KSP;
+using KSPAchievements;
+using Strategies;
+using Strategies.Effects;
+using ContractConfigurator;
+
+namespace Strategia
+{
+    public class ContractWeightBooster : StrategyEffect
+    {
+        private List<CelestialBody> bodies;
+        private bool bonusGiven = false;
+        private int weight;
+
+        public ContractWeightBooster(Strategy parent)
+            : base(parent)
+        {
+        }
+
+        protected override string GetDescription()
+        {
+            return "Increases likelihood of receiving contracts for " + CelestialBodyUtil.BodyList(bodies, "and") + ".";
+        }
+
+        protected override void OnLoadFromConfig(ConfigNode node)
+        {
+            base.OnLoadFromConfig(node);
+
+            bodies = ConfigNodeUtil.ParseValue<List<CelestialBody>>(node, "bodies");
+            weight = ConfigNodeUtil.ParseValue<int>(node, "weight");
+        }
+
+        protected override void OnRegister()
+        {
+            // Check for an activation
+            if (Parent.IsActive && !bonusGiven)
+            {
+                bonusGiven = true;
+                foreach (CelestialBody body in bodies)
+                {
+                    Contracts.ContractSystem.WeightAdjustment(body.name, weight);
+                }
+            }
+        }
+
+        protected override void OnUnregister()
+        {
+            // Check for a deactivation
+            if (!Parent.IsActive)
+            {
+                foreach (CelestialBody body in bodies)
+                {
+                    Contracts.ContractSystem.WeightAdjustment(body.name, -weight);
+                }
+            }
+        }
+
+        protected override void OnSave(ConfigNode node)
+        {
+            base.OnSave(node);
+
+            node.AddValue("bonusGiven", bonusGiven);
+        }
+
+        protected override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+
+            bonusGiven = ConfigNodeUtil.ParseValue<bool>(node, "bonusGiven");
+        }
+    }
+}
