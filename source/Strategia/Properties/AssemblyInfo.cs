9b88a3453771b089b4d6df65ee817405e0aa38d2
Jeremie Rossignol <jrossignol@gmail.com>
2019-10-22 10:50:31 -0600

    1.8.0

--- source/Strategia/Properties/AssemblyInfo.cs
+++ source/Strategia/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.7.3")]
-[assembly: AssemblyInformationalVersion("1.7.3")]
+[assembly: AssemblyFileVersion("1.8.0")]
+[assembly: AssemblyInformationalVersion("1.8.0")]
 [assembly: KSPAssembly("Strategia", 1, 0)]
